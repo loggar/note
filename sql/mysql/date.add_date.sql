534f567da511fe3926dd658cf6dc67c2d4e942bd
charly.lee <webnl@DT-Charly.koi.local>
2019-05-01 13:51:57 +1000

    feat (sql) DATE_ADD

---
+++ sql/mysql/date.add_date.sql
@@ -0,0 +1,5 @@
+SELECT DATE_ADD('2008-05-15', INTERVAL 10 DAY) as required_date;
+
+SELECT DATE_ADD('2008-05-15', INTERVAL 2 MONTH) as required_date;
+
+SELECT DATE_ADD('2008-05-15', INTERVAL 1 YEAR) as required_date;
