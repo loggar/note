7813ebb5b91621a51a7eb4a1ef8370cbad423bc6
charly.lee <webnl@DT-Charly.koi.local>
2019-05-13 14:56:08 +1000

    feat (powershell) practices

--- powershell/multi-dimensional-array.ps1
+++
@@ -1,9 +0,0 @@
-$Colors="Red","Yellow","Orange","Green"
-$Colors
-$Colors[0]
-$Colors += "Purple"
-$Colors
-
-$Marshmallows = @(("Pink","Yellow","Orange","Green","Blue"),("Hearts","Stars","Moons","Clovers","Diamonds"))
-$Marshmallows
-$Marshmallows[1][2]
